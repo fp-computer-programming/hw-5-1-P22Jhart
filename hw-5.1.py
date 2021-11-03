8ec46d429d5d8ca1c7d5f1fe45dd9a3a3353778e
Jordan Hart <p22jhart@fairfieldprep.org>
2021-11-02 20:34:04 -0400

    hw

--- hw-5.1.py
+++ hw-5.1.py
@@ -4,9 +4,7 @@ import random
 
 print(random.randint(30,50))
 
-random.seed (1)
 
-print(random.randrange(30,50))
 
 #question 2
 
@@ -48,25 +46,33 @@ print(random.randrange(0,20))
 
 #question 6
 
-print(random.randrange('cat', 'dog', 'sheep', 'cow', 'chicken', 'pig'))
+table = ['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig']
+animal = random.choice(table)
+
+
+
 
 #question 7
+randomnum= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
-
+print(random.choices(randomnum,k = 4))
 
 
 #question 8
-
-
-
-
+randomnum5 = [1,2,3,4,5]
+print(random.choices(randomnum5, k=5))
 
 
 #question 9
+card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
+card_gen = random.choice(card)
+print(card_gen)
 
-cards = '(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52)'
 
-random.shuffle(cards)
+#question 10
 
-print(cards)
+random.seed(1942)
+random.randint(1,1000)
+ranonumber=(random.randint(1,1000))
+print(ranonumber)
+random.seed (1942)
